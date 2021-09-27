5b92369b05eb6f5efacb555d4808af4a9b4fa800
JungSangHyeon <dev.jusa@gmail.com>
2021-09-27 15:23:48 +0900

    Observable Value & Callback Bind Example Finished

--- app/src/main/java/com/example/databindingexample/MainActivity.kt
+++ app/src/main/java/com/example/databindingexample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.databindingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableField
+import com.example.databindingexample.databinding.ActivityMain3Binding
 
 class MainActivity : AppCompatActivity() {
+
+    val text = ObservableField("Random Number")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main3)
+
+        val binding: ActivityMain3Binding = DataBindingUtil.setContentView(this, R.layout.activity_main3)
+        binding.mainActivity = this
+    }
+
+    fun onButtonPressed() {
+        text.set((0..10).random().toString())
     }
 }
